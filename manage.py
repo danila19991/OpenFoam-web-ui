558125da812d3f9ebe3eb902410e5019ed0cbeeb
danila19991 <guseffdanila@ya.ru>
2020-02-22 15:44:56 +0300

    rename project

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dobetter.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'remoteexperements.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
